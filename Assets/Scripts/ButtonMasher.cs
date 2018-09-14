cd4ff129fa17e0a03515e1fdb6c5fa2e4a6edabc
Hussar99 <43067400+Hussar99@users.noreply.github.com>
2018-09-14 14:50:19 +0100

    Adding timer mechanics.

--- Assets/Scripts/ButtonMasher.cs
+++ Assets/Scripts/ButtonMasher.cs
@@ -7,18 +7,29 @@ public class ButtonMasher : MonoBehaviour {
     //Data for our class
     public AudioSource clickSound; //sound that will play when the button will be clicked
     public TextMesh scoreText; //Display text for the player score
+    public TextMesh timerText; //Display text for the time remaining
+    public float gameLength; // how many seconds will the game last
 
     private int score = 0; //the numerical data value of our score
+    private float timeRemaining = 0; //Numerical time remaining for completion of the game
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("Start Method called");
+
+        timeRemaining = gameLength;
 	}//End of Start ()
 	
 	// Update is called once per frame
 	void Update ()
     {
         Debug.Log("Update called");
+
+        //Update the numerical time remaining
+        timeRemaining = timeRemaining - Time.deltaTime;
+
+        //update the visual time remaining
+        timerText.text = timeRemaining.ToString();
     }//End of Update ()
 
     void OnMouseDown()
